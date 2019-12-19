d7e6e9681f5c09d9ff951cb321cacbd834d1e9df
ChrisK197 <chrkal22@bergen.org>
2019-12-19 11:42:19 -0500

    added background image and music (might want to change music idk)

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,9 +1,13 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -14,12 +18,25 @@ import java.util.Scanner;
 
 
 public class MainFile extends Application {
+
+    private String musicFile = "Waiting.mp3";
+    private Media sound = new Media(new File(musicFile).toURI().toString());
+    private MediaPlayer mediaPlayer = new MediaPlayer(sound);
+
     public void start(Stage ps) throws FileNotFoundException {
         BorderPane mainPane = new BorderPane();
         HBox bottomPane = new HBox();
         Text questionBox = new Text();
         mainPane.setBottom(bottomPane);
         mainPane.setCenter(questionBox);
+
+        Image background = new Image("background.png");
+        ImageView imageView = new ImageView(background);
+        imageView.fitWidthProperty().bind(mainPane.widthProperty());
+        imageView.fitHeightProperty().bind(mainPane.heightProperty());
+        mainPane.getChildren().add(imageView);
+
+        /*throws a file not found exception
         Scanner s = new Scanner(new File("questions.txt"));
         HashMap<String, String> qDict = new HashMap<>();
         while (s.hasNext()){
@@ -27,14 +44,15 @@ public class MainFile extends Application {
             String[] list = str.split("::");
             qDict.put(list[0], list[1]);
         }
+        */
 
-
-
-
-
+        //I thought some classical music would be nice, we can change it later tho
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.setVolume(100);
+        mediaPlayer.play();
 
         Scene scene = new Scene(mainPane, 1200,600);
-        ps.setTitle("Quiz Bowl!!");
+        ps.setTitle("Quiz Bowl");
         ps.setScene(scene);
         ps.show();
     }
