f3de827a9f1f3f338eb80908c95ef3a4364437e5
Leoc74 <lciccarelli74@gmail.com>
2019-12-19 08:56:39 -0500

    helllo

--- src/MainFile.java
+++ src/MainFile.java
@@ -7,15 +7,28 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class MainFile extends Application {
-    public void start(Stage ps){
+    public void start(Stage ps) throws FileNotFoundException {
         BorderPane mainPane = new BorderPane();
         HBox bottomPane = new HBox();
         Text questionBox = new Text();
         mainPane.setBottom(bottomPane);
         mainPane.setCenter(questionBox);
+        Scanner s = new Scanner(new File("questions.txt"));
+        HashMap<String, String> qDict = new HashMap<>();
+        while (s.hasNext()){
+            String str = s.nextLine();
+            String[] list = str.split("::");
+            qDict.put(list[0], list[1]);
+        }
+
+
 
 
 
