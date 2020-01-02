ee3fb5271e538dd5877fa7b7a45a954d75b0ca6a
Batman0016 <evacic22@bergen.org>
2020-01-02 08:55:26 -0500

    hi

--- src/MainFile.java
+++ src/MainFile.java
@@ -13,6 +13,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -35,9 +36,14 @@ public class MainFile extends Application {
         imageView.fitWidthProperty().bind(mainPane.widthProperty());
         imageView.fitHeightProperty().bind(mainPane.heightProperty());
         mainPane.getChildren().add(imageView);
-        //
 
-        /*throws a file not found exception
+        Image contestant = new Image("contestant.jpg");
+        ImageView contestantImageView = new ImageView(contestant);
+        contestantImageView.setFitWidth(contestant.getWidth()/2);
+        contestantImageView.setFitHeight(contestant.getHeight()/2);
+        bottomPane.getChildren().add(contestantImageView);
+        bottomPane.toFront();
+
         Scanner s = new Scanner(new FileReader("questions.txt"));
         HashMap<String, String> qDict = new HashMap<>();
         while (s.hasNext()){
@@ -45,7 +51,6 @@ public class MainFile extends Application {
             String[] list = str.split("::");
             qDict.put(list[0], list[1]);
         }
-        */
 
         //I thought some classical music would be nice, we can change it later tho
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
