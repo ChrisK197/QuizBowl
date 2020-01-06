9d7eb3a8622eecc05d886cddeea5a6217d1f431f
Batman0016 <evacic22@bergen.org>
2020-01-06 11:30:00 -0500

    hi

--- src/MainFileTester.java
+++ src/MainFileTester.java
@@ -1,21 +1,17 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.paint.Color;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -25,37 +21,34 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 
-public class MainFile extends Application {
+public class MainFileTester extends Application {
 
     private String musicFile = "Waiting.mp3";
     private Media sound = new Media(new File(musicFile).toURI().toString());
     private MediaPlayer mediaPlayer = new MediaPlayer(sound);
 
     public void start(Stage ps) throws FileNotFoundException {
-        BorderPane mainPane = new BorderPane();
-        HBox bottomPane = new HBox();
+        Pane mainPane = new Pane();
         Text questionBox = new Text();
-        mainPane.setBottom(bottomPane);
-        mainPane.setCenter(questionBox);
+        mainPane.getChildren().add(questionBox);
 
-        BackgroundImage myBI= new BackgroundImage(new Image("background.png"),
-                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
-                BackgroundSize.DEFAULT);
-        mainPane.setBackground(new Background(myBI));
+        /*Image background = new Image("background.png");
+        ImageView imageView = new ImageView(background);
+        imageView.fitWidthProperty().bind(mainPane.widthProperty());
+        imageView.fitHeightProperty().bind(mainPane.heightProperty());
+        mainPane.getChildren().add(imageView);*/
 
-
-        /*Image contestant1 = new Image("contestant1.jpg");
+        Image contestant1 = new Image("contestant1.jpg");
         ImageView contestant1imageView = new ImageView(contestant1);
         contestant1imageView.setFitWidth(contestant1.getWidth()/2);
         contestant1imageView.setFitHeight(contestant1.getHeight()/2);
-        bottomPane.getChildren().add(contestant1imageView);
-//        bottomPane.toFront();
+        mainPane.getChildren().add(contestant1imageView);
 
         Image contestant2 = new Image("contestant2.jpg");
         ImageView contestant2imageView = new ImageView(contestant2);
         contestant2imageView.setFitWidth(contestant2.getWidth()/2);
         contestant2imageView.setFitHeight(contestant2.getHeight()/2);
-        bottomPane.getChildren().add(contestant2imageView);*/
+        mainPane.getChildren().add(contestant2imageView);
 
         Rectangle qBox = new Rectangle(150, 75, 900, 100);
         qBox.setFill(Color.WHITE);
@@ -79,7 +72,7 @@ public class MainFile extends Application {
         mediaPlayer.setVolume(100);
         mediaPlayer.play();
 
-        Scene scene = new Scene(mainPane, 955,598);
+        Scene scene = new Scene(mainPane, 1200,600);
         ps.setTitle("Quiz Bowl");
         ps.setScene(scene);
         ps.show();
